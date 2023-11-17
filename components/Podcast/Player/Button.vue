6b115228c7205f28fbb18294991e4ef3dc19f898
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-11-17 16:38:59 +0100

    Working on the button

--- components/Podcast/Player/Button.vue
+++ components/Podcast/Player/Button.vue
@@ -1,6 +1,30 @@
 <template>
-  <div @click="startAudio">
+  <div class="audio-container">
     <AVCircle v-bind="audioVisualConfig"/>
+    <div @click="toggleAudio" class="play-button">
+      <!-- Play/Pause button SVG -->
+      <svg
+          v-if="data.isPlaying"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+      >
+        <rect x="4" y="4" width="6" height="16"></rect>
+        <rect x="14" y="4" width="6" height="16"></rect>
+      </svg>
+      <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+      >
+        <polygon points="5 3 19 12 5 21 5 3"></polygon>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -16,14 +40,48 @@ const audioVisualConfig = {
   barColor: 'red',
   outlineColor: 'red',
   playtimeColor: 'red',
-  playtimeFont: '48px monospace',
+  playtimeFont: '1px monospace',
   rotateGraph: true,
   playtime: false,
   progress: false,
-//  audioControls: false, // TODO: Comment this line to test audio
+  audioControls: false, // TODO: Comment this line to test audio
 };
 
-const startAudio = () => { // TODO: Fix this
-//  audioVisualConfig.audioControls = !audioVisualConfig.audioControls;
+const data = reactive({
+  isPlaying: false,
+  audio: new Audio('/music.mp3')
+});
+
+const toggleAudio = () => {
+  // Toggle the play/pause state
+  if (!data.isPlaying) {
+    data.audio.play();
+  } else {
+    data.audio.pause();
+  }
+
+  data.isPlaying = !data.isPlaying;
 };
 </script>
+
+<style scoped>
+.audio-container {
+  position: relative;
+}
+
+.play-button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80px;
+  height: 80px;
+  cursor: pointer;
+}
+
+.play-button svg {
+  width: 100%;
+  height: 100%;
+  fill: red;
+}
+</style>
