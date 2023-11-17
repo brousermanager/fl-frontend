1d0fa36696bfb77ff6371d61a147d4d8fca42e4b
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-11-17 13:31:11 +0100

    Improving graphics

--- components/Podcast/Player/Button.vue
+++ components/Podcast/Player/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <AVCircle src="/music.mp3"/>
+  <AVCircle src="/music.mp3" :canv-width="300" :canv-height="300" outline-color="rgb(255, 0, 0)"
+            bar-color="rgb(255, 0, 0)" progress-color="rgb(255, 0, 0)" playtime-color="rgb(255, 0, 0)"
+            playtime-font="32px Monaco" :rotate-graph="true"/>
 </template>
 
 <script setup lang="ts">
