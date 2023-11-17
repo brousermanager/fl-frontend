8163d26b26798c86f44e91b48e70b681a7638d8e
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-11-17 13:31:11 +0100

    Improving play button graphics

--- components/Podcast/Player/Button.vue
+++ components/Podcast/Player/Button.vue
@@ -1,5 +1,6 @@
 <template>
-  <AVCircle src="/music.mp3"/>
+  <AVCircle src="/music.mp3" :canv-width="250" :canv-height="250" outline-color="red" bar-color="red"
+            progress-color="red" playtime-color="red" playtime-font="48px monospace" :rotate-graph="true"/>
 </template>
 
 <script setup lang="ts">
