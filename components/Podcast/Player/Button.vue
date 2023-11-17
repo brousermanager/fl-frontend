47dae45699bd0f0b09177c6579a6f33396ea4a74
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-11-17 14:07:55 +0100

    Working on the button

--- components/Podcast/Player/Button.vue
+++ components/Podcast/Player/Button.vue
@@ -1,19 +1,29 @@
 <template>
-  <AVCircle v-bind="audioVisualConfig"/>
+  <div @click="startAudio">
+    <AVCircle v-bind="audioVisualConfig"/>
+  </div>
 </template>
 
 <script setup lang="ts">
 import {AVCircle} from 'vue-audio-visual'
 
+// TODO: Check https://www.npmjs.com/package/vue-audio-visual
+
 const audioVisualConfig = {
   src: '/music.mp3',
   canvWidth: 250,
   canvHeight: 250,
-  outlineColor: 'red',
   barColor: 'red',
-  progressColor: 'red',
+  outlineColor: 'red',
   playtimeColor: 'red',
   playtimeFont: '48px monospace',
   rotateGraph: true,
+  playtime: false,
+  progress: false,
+//  audioControls: false, // TODO: Comment this line to test audio
+};
+
+const startAudio = () => { // TODO: Fix this
+//  audioVisualConfig.audioControls = !audioVisualConfig.audioControls;
 };
 </script>
