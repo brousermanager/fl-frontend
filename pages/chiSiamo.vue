235e13e7ff777dfd8b16727c0571b3d79fd9a204
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-07-01 19:24:56 +0200

    chi siamo

--- pages/chiSiamo.vue
+++ pages/chiSiamo.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <h1>Hello, World!</h1>
-  </div>
+  <v-row>
+    <v-col cols="12" md="6">
+      <v-img src="/chiSiamo.jpg" style="height: 100vh;" />
+    </v-col>
+    <v-col cols="12" md="6">
+      <v-container>
+        <h1>
+          Questo è un titolo
+        </h1><br>
+        <h3>
+          Questo invece è un sottotitolo
+        </h3><br>
+        <p>
+          Qui invece c'è anche un testo più lungo. Così si può scrivere la lista della spesa, ma anche il cazzo che me ne frega,
+          perché questo è lo spazio libero, ceh il corpo, il testo, lu body. Insomma cher è.
+        </p><br>
+        <v-btn text="Bottone" />
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
-  name: "Pagina1",
-};
+  //
 </script>
