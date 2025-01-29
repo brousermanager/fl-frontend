9065a72fe9697dc9d654fba502dd0e7afb897bad
edoardoPomarico <edoardo.pomarico@gmail.com>
2025-01-29 21:04:12 +0100

    feat: add icons test

---
+++ pages/test.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-row no-gutters>
+        <v-col
+          cols="4"
+          sm="4"
+        >
+            <v-btn
+                class="ma-2"
+                color="purple"
+                icon="mdi-twitter"
+            >
+            </v-btn>
+        
+            <v-btn
+                class="ma-2"
+                color="blue-darken-2"
+                icon="mdi-twitch"
+            ></v-btn>
+    
+            <v-btn
+                class="ma-2"
+                color="purple-darken-2"
+                icon="mdi-instagram"
+            ></v-btn>
+        
+            <v-btn
+                class="ma-2"
+                color="blue-grey-darken-2"
+                icon="mdi-facebook"
+            >
+            </v-btn>
+        </v-col>
+        
+    </v-row>
+  </template>
+  //hover icone ig, twitch, etc
+  // @click reindirizza alle diverse pagine
+  // bottoni nell'header 
