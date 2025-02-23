87126ec275a9f5001807b94e39a6974d45218325
Domenico Devito <domenico@ambienttechnologies.xyz>
2025-02-23 13:53:02 +0100

    refactor: adjust Hero component parallax height from 50vh to 40vh

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-parallax src="bg.png" height="50vh">
+  <v-parallax src="bg.png" height="40vh">
     <div class="d-flex flex-column fill-height justify-center align-center text-white">
       <h1 class="text-h2 font-weight-thin mb-4">Radio Frequenza Libera</h1>
       <h2 class="subheading">
-        Stai ascoltando:
+        Stai ascoltando: <br />
         {{ store.currentPodcast.title }}
       </h2>
     </div>
