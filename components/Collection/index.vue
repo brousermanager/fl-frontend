0818c0aafd6a7f69d172f08a9aff80a07ac9155e
keewee <kinycx@gmail.com>
2025-04-06 16:06:01 +0200

    fix: remove log

--- components/Collection/index.vue
+++ components/Collection/index.vue
@@ -4,7 +4,6 @@
       <v-card elevation="8">
         <v-card-title>LE NOSTRE RUBRICHE</v-card-title>
       </v-card>
-      {{ store.podcastCollections }}
     </v-col>
     <v-col cols="12" md="6" v-for="(collection, index) in store.podcastCollections" :key="index">
         <CollectionCard :podcastCollection="collection" />
