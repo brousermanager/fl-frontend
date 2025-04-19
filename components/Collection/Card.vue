d250aca22ceefd5c2bf77ab438c6b5d840db7d67
keewee <kinycx@gmail.com>
2025-04-20 00:38:46 +0200

    fix: update PodcastCollection model to use 'cover' instead of 'cover_url' and adjust Card.vue accordingly

--- components/Collection/Card.vue
+++ components/Collection/Card.vue
@@ -2,7 +2,7 @@
   <v-card elevation="8" class="ma-5" hover>
     <v-row @click="redirectToCollectionPage(podcastCollection.id)">
       <v-col cols="12">
-        <v-img height="225" :src="podcastCollection.cover_url" cover></v-img>
+        <v-img height="225" :src="podcastCollection.cover" cover></v-img>
       </v-col>
       <v-col cols="12">
         <v-card-item>
