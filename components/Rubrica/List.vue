1430d7ff57db817e5228f5f886aaf018ed6c2848
Emanuele Colonna <colonnamanu98@gmail.com>
2025-05-06 20:22:58 +0200

    feat style for list collection

--- components/Rubrica/List.vue
+++ components/Rubrica/List.vue
@@ -1,26 +1,31 @@
 <template>
   <v-list variant="plain" rounded select-strategy="leaf" elevation="4">
-    <v-list-item
-      v-for="(podcast, index) in props.podcasts"
-      :key="podcast.id"
-      :active="store.currentPodcast.id === podcast.id"
-      :active-color="store.currentPodcast.id === podcast.id ? 'secondary' : ''"
-      active-class="selected-item"
-      class="py-3"
-      @click="updateCurrentPodcast(podcast.id)"
-    >
-      <template v-slot:prepend>
-        <v-avatar color="pink" size="36">
-          <span class="text-caption">{{ index + 1 }}</span>
-        </v-avatar>
-      </template>
+    <template v-for="(podcast, index) in props.podcasts" :key="podcast.id">
+      <v-list-item
+        :active="store.currentPodcast.id === podcast.id"
+        :active-color="store.currentPodcast.id === podcast.id ? 'secondary' : ''"
+        active-class="selected-item"
+        class="py-3"
+        @click="updateCurrentPodcast(podcast.id)"
+      >
+        <template v-slot:prepend>
+          <v-avatar
+            :color="store.currentPodcast.id === podcast.id ? 'secondary' : 'pink'"
+            size="36"
+          >
+            <span class="text-caption-bold">{{ index + 1 }}</span>
+          </v-avatar>
+        </template>
 
-      <v-list-item-title>{{ podcast.title }}</v-list-item-title>
+        <v-list-item-title>{{ podcast.title }}</v-list-item-title>
 
-      <v-list-item-subtitle class="text-high-emphasis">
-        {{ podcast.description }}
-      </v-list-item-subtitle>
-    </v-list-item>
+        <v-list-item-subtitle class="text-high-emphasis">
+          {{ podcast.description }}
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-divider v-if="index < props.podcasts.length - 1"></v-divider>
+    </template>
     <v-spacer></v-spacer>
   </v-list>
 </template>
