171262899b8822805547472f0e9348b905f2620d
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-07-26 14:32:43 +0200

    feat: mobile bottom navigation

--- app/components/Layout/RailDrawer.vue
+++ app/components/Layout/RailDrawer.vue
@@ -1,39 +1,42 @@
 <template>
   <v-app-bar scroll-behavior="hide" >
     <v-app-bar-nav-icon
+      v-if="!$vuetify.display.mobile"
       variant="text"
       @click.stop="drawer = !drawer"
     />
 
-    <v-toolbar-title><strong>Radio Frequenza Libera</strong></v-toolbar-title>
+    <v-toolbar-title><strong>
+      Radio Frequenza Libera
+    </strong></v-toolbar-title>
   </v-app-bar>
 
   <v-navigation-drawer
+    v-if="!$vuetify.display.mobile"
     v-model="drawer"
-    :location="$vuetify.display.mobile ? 'bottom' : undefined"
     temporary
   >
     <v-list density="compact" nav>
       <v-list-item
-        prepend-icon="mdi-home-city"
-        title="Home"
-        value="home"
-        @click="navigateTo('/')"
-      />
-      <v-list-item
-        prepend-icon="mdi-podcast"
-        title="Rubriche"
-        value="rubriche"
-        @click="navigateTo('/rubriche')"
-      />
-      <v-list-item
-        prepend-icon="mdi-account-group"
-        title="Staff"
-        value="staff"
-        @click="navigateTo('/staff')"
+        v-for="option in menuOptions"
+        :prepend-icon="option.icon"
+        :title="option.title"
+        :value="option.path"
+        @click="navigateTo(option.path)"
       />
     </v-list>
   </v-navigation-drawer>
+
+  <v-bottom-navigation v-else>
+    <v-btn
+      v-for="option in menuOptions"
+      :value="option.path"
+      @click="navigateTo(option.path)"
+    >
+      <v-icon>{{ option.icon }}</v-icon>
+      <span>{{ option.title }}</span>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script lang="ts" setup>
@@ -43,4 +46,10 @@ const group = ref("");
 watch(group, () => {
   drawer.value = false;
 });
+
+const menuOptions = [
+  { title: "Home", icon: "mdi-home-city", path: "/" },
+  { title: "Rubriche", icon: "mdi-podcast", path: "/rubriche" },
+  { title: "Staff", icon: "mdi-account-group", path: "/staff" },
+];
 </script>
