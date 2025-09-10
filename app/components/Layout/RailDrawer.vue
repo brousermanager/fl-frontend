2a7b66678f8edd27e236e6532d002f0d7ba11db1
keewee <kinycx@gmail.com>
2025-09-10 12:14:07 +0200

    refactor: clean up RailDrawer component layout and improve code consistency

--- app/components/Layout/RailDrawer.vue
+++ app/components/Layout/RailDrawer.vue
@@ -1,26 +1,19 @@
 <template>
-  <v-app-bar scroll-behavior="hide" >
+  <v-app-bar scroll-behavior="hide">
     <v-app-bar-nav-icon
       v-if="!$vuetify.display.mobile"
       variant="text"
       @click.stop="drawer = !drawer"
     />
 
-    <v-toolbar-title><strong>
-      Radio Frequenza Libera
-    </strong></v-toolbar-title>
-
-    <v-img :src="logo" max-width="50" class="mr-5" />
+    <v-img :src="logo" class="mx-4" />
   </v-app-bar>
 
-  <v-navigation-drawer
-    v-if="!$vuetify.display.mobile"
-    v-model="drawer"
-    temporary
-  >
+  <v-navigation-drawer v-if="!$vuetify.display.mobile" v-model="drawer" temporary>
     <v-list density="compact" nav>
       <v-list-item
         v-for="option in menuOptions"
+        :key="option.title"
         :prepend-icon="option.icon"
         :title="option.title"
         :value="option.path"
@@ -29,20 +22,20 @@
     </v-list>
   </v-navigation-drawer>
 
-  <v-bottom-navigation v-else>
+  <v-bottom-navigation v-else grow app :order="-1">
     <v-btn
       v-for="option in menuOptions"
+      :key="option.title"
       :value="option.path"
       @click="navigateTo(option.path)"
     >
       <v-icon>{{ option.icon }}</v-icon>
-      <span>{{ option.title }}</span>
     </v-btn>
   </v-bottom-navigation>
 </template>
 
 <script lang="ts" setup>
-import logo from '@/assets/logo.png';
+import logo from "@/assets/logo.png";
 
 const drawer = ref(false);
 const group = ref("");
