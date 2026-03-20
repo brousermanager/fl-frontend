995d9026cc6500d5e15cc210944373b534612cc7
Emanuele Colonna <colonnamanu98@gmail.com>
2026-03-20 17:20:33 +0100

    feat: adjust iframe styling in statuto page for improved layout

--- app/pages/statuto.vue
+++ app/pages/statuto.vue
@@ -2,6 +2,6 @@
 	<iframe
 		src="statuto031020231312.pdf#view=FitH"
 		title="Statuto PDF"
-		style="display: block; width: 100%; height: calc(100dvh - var(--v-layout-top, 0px)); border: 0;"
+		style="display: block; width: 80%; height: calc(100dvh - var(--v-layout-top, 0px)); border: 0; margin: 0 auto;"
 	/>
 </template>
